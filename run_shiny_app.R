e7cd2552b2a2e05ad981315595e4c06213bb908c
jacopo <j.ac@hotmail.it>
2016-10-02 19:14:34 +0200

    small comment update
    tiny comment update, just to get familiar with commits :)

--- run_shiny_app.R
+++ run_shiny_app.R
@@ -3,5 +3,6 @@ message('library paths:\n', paste('... ', .libPaths(), sep='', collapse='\n'))
 
 # set working directory as the directory where this file is
 # setwd("C:/Users/pc/Documents/github/shinyapp_insurance_dashboard")
+# could not work with old version of shiny
 
 shiny::runApp('./shiny/')
